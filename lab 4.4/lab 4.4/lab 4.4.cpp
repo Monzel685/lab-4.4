df6075f87c941d76d3d56b86cb1cb30d96a8566d
Monzel685 <vitalijpivnenko418@gmail.com>
2024-09-24 17:21:31 +0300

    Add files via upload

---
+++ lab 4.4/lab 4.4/lab 4.4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, y;
+	double R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "y" << "    |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -1 - R)
+			y = -1 * (x - 1 - R);
+		else
+			if (x > -1 - R && x < -1)
+				y = sqrt(-(x * x) - 2 * x * R);
+			else
+				if (x > -1 && x < 1)
+					y = R;
+				else
+					if (x > 1 && x < 2)
+						y = -x * R - x + 2 * R + 1;
+					else
+						y = -1;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
